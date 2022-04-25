19c6ce08bece6ba16dd3c0404d25baccc9715dd7
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-04-25 13:17:06 -0300

    GBP price

--- motor.js
+++ motor.js
@@ -337,7 +337,7 @@ const blockPriceAuto = (coin,price,sgl) => {
 blockPriceAuto(eurv1,'5.98','EUR')
 //blockPriceAuto(usdv1,'5.20','USD')
 blockPriceAuto(cadv1,'4.50','CAD')
-blockPriceAuto(gbpv1,'6.72','GBP')
+//blockPriceAuto(gbpv1,'6.72','GBP')
 
 
 
