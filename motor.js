784fb28523e91e28ca4491fa12c7f4cdbec98f6e
Anderson Siqueira <92267959+andersoncsiqueira@users.noreply.github.com>
2022-09-23 12:50:03 -0300

    eur e cad

--- motor.js
+++ motor.js
@@ -307,9 +307,9 @@ const blockPriceAuto = (coin,price,sgl) => {
 
 // Preços travados
 
-//blockPriceAuto(eurv1,'5.60','EUR')
+blockPriceAuto(eurv1,'5.56','EUR')
 //blockPriceAuto(usdv1,'5.54','USD')
-//blockPriceAuto(cadv1,'4.50','CAD')
+blockPriceAuto(cadv1,'4.50','CAD')
 blockPriceAuto(gbpv1,'6.99','GBP')
 
 
